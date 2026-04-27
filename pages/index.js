10d9be5e90cd6d3ba786aaea618704928890a8fa
amen70241-cloud <amen70241@gmail.com>
2026-04-27 19:45:45 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1091,7 +1091,6 @@ const whatsappLink = `https://wa.me/${cleanWhatsApp}?text=Hello%20Perfect%20Foun
   <span className="absolute inset-0 rounded-full bg-[#25D366] opacity-30 animate-ping"></span>
   <span className="relative z-10">💬</span>
 </a>
-<SchoolChatbot />
 </main>
 );
 }
